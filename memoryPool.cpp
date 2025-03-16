f76870228b8d2beea1067f6a8b7492e0b9e878d7
vldzyzy <vldzyzy@qq.com>
2025-03-16 08:25:06 +0000

    v1.1

--- memoryPool.cpp
+++ memoryPool.cpp
@@ -22,43 +22,69 @@ void MemoryPool::init(size_t slotSize) {
     SlotSize_ = slotSize;
     firstBlock_ = nullptr;
     curSlot_ = nullptr;
-    freeList_ = nullptr;
+    freeList_.store(nullptr, std::memory_order_relaxed);
     lastSlot_ = nullptr;
 }
 
+// 实现无锁入栈操作
+bool MemoryPool::pushFreeList(Slot* slot) {
+    while(true) {
+        // 获取当前头节点
+        Slot* oldHead = freeList_.load(std::memory_order_relaxed);
+        // 将新节点的 next 指向当前头节点
+        slot->next.store(oldHead, std::memory_order_relaxed);
+
+        // 尝试将新节点设置为头节点
+        if(freeList_.compare_exchange_weak(oldHead, slot,
+                                            std::memory_order_release,
+                                            std::memory_order_relaxed))  
+        {
+            return true;
+        }
+        // CAS 失败重试
+    }
+}
+
+Slot* MemoryPool::popFreeList() {
+    while(true) {
+        // 获取当前头节点
+        Slot* oldHead = freeList_.load(std::memory_order_relaxed);
+        if(oldHead == nullptr) return nullptr; // 栈为空
+        // 获取下一个节点
+        Slot* newHead = oldHead->next.load(std::memory_order_relaxed);
+
+        // 尝试将下一个节点设为头节点
+        if(freeList_.compare_exchange_weak(oldHead, newHead,
+                                            std::memory_order_acquire,
+                                            std::memory_order_relaxed))
+        {
+            return oldHead;
+        }
+        // CAS失败重试
+    }
+}
+
+
 void* MemoryPool::allocate() {
     // 优先使用空闲链表中的内存槽
-    if(freeList_ != nullptr) {
-        {
-            std::lock_guard<std::mutex> lock(mutexForFreeList_);
-            if(freeList_ != nullptr) {
-                Slot* tmp = freeList_;
-                freeList_ = freeList_->next;
-                return tmp;
-            }
-        }
-    }
+    Slot* slot = popFreeList();
+    if(slot != nullptr) return slot;
 
-    Slot* tmp;
-    {
-        std::lock_guard<std::mutex> lock(mutexForBlock_);
-        if(curSlot_ >= lastSlot_) {
-            // 当前内存块已无内存槽可用，开辟一块新的内存块
-            allocateNewBlock();
-        }
+    // 如果空闲链表为空,则分配新的内存
+    std::lock_guard<std::mutex> lock(mutexForBlock_);
+    if(curSlot_ > lastSlot_) allocateNewBlock();
 
-        tmp = curSlot_;
-        curSlot_ += SlotSize_ / sizeof(Slot);  // 指针加1 -> 下8个字节。
-    }
-    return tmp;
+    // 返回指针, 更新内存池
+    Slot* result = curSlot_;
+    curSlot_ = reinterpret_cast<Slot*>(reinterpret_cast<char*>(curSlot_) + SlotSize_);
+    return result;
 }
 
 void MemoryPool::deallocate(void* ptr) {
     if(ptr) {
         // 回收内存，将内存通过头插法插入到空闲链表中
-        std::lock_guard<std::mutex> lock(mutexForFreeList_);
-        reinterpret_cast<Slot*>(ptr)->next = freeList_;
-        freeList_ = reinterpret_cast<Slot*>(ptr);
+        Slot* slot = static_cast<Slot*>(ptr);
+        pushFreeList(slot);
     }
 }
 
